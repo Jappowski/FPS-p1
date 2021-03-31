e1454e0b889e390a600eb5cf98fd7fe7b94eb377
Jappowski <58007440+Jappowski@users.noreply.github.com>
2021-03-31 19:51:06 +0200

    Add reload system
    reload and simple animation for it

--- Assets/_scripts/GunShot.cs
+++ Assets/_scripts/GunShot.cs
@@ -1,5 +1,7 @@
 
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GunShot : MonoBehaviour
 {
@@ -8,21 +10,53 @@ public class GunShot : MonoBehaviour
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject hitEffect;
+    public Animator animator;
     public float impactForce = 100f;
 
     private float nextShoot = 0f;
+
+    public float currentAmmo;
+    private float maxAmmo = 30f;
+    public float reloadTime = 3f;
+    private bool isReloading = false;
+    void start()
+    {
+        currentAmmo = maxAmmo;
+    }
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+            return;
+        
+        if (currentAmmo <= 0 || Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButton("Fire1") && Time.time >= nextShoot)
         {
             nextShoot = Time.time + 1f / fireRate;
             Shoot();
         }
     }
+  
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        animator.SetBool("Reload", true);
+        yield return new WaitForSeconds(reloadTime-.25f);
+        animator.SetBool("Reload", false);
+        yield return new WaitForSeconds(.25f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+        animator.SetBool("Reload", false);
+        
+    }
 
     void Shoot()
     {
+        currentAmmo--;
         muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
