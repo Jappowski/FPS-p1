e31c7e6a7fd6f8451d2bbdfb9a25d6d1f53a6f5b
viktord8888 <viktordukacz@onet.pl>
2022-01-19 17:41:30 +0100

    added hitmarker

--- Assets/_scripts/HUD.cs
+++ Assets/_scripts/HUD.cs
@@ -4,48 +4,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HUD : MonoBehaviour
-{
+public class HUD : MonoBehaviour {
     [SerializeField] public GameObject StartUi;
     [SerializeField] public GameObject StopUi;
     [SerializeField] public GameObject InGameHUD;
     [SerializeField] public Text HP;
-    private void Awake()
-    {
+
+    private void Awake() {
         HandleEvents();
         StartUi.SetActive(true);
     }
 
-    private void HandleEvents()
-    {
+    private void HandleEvents() {
         GameEvents.onEscClick += OnEscClickHandle;
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         GameEvents.onEscClick -= OnEscClickHandle;
     }
 
-    private void OnEscClickHandle()
-    {
-        if (GameManager.instance.gameState == GameManager.GameState.InGame)
-        {
+    private void OnEscClickHandle() {
+        if (GameManager.instance.gameState == GameManager.GameState.InGame) {
             GameEvents.BroadcastOnGameStateChange(GameManager.GameState.Stop);
             StopUi.SetActive(true);
         }
-        else
-        {
+        else {
             GameEvents.BroadcastOnGameStateChange(GameManager.GameState.InGame);
             StopUi.SetActive(false);
         }
     }
 
-    public void HostLobbyButton()
-    {
+    public void HostLobbyButton() {
         GameEvents.BroadcastOnGameStateChange(GameManager.GameState.InGame);
     }
-    public void LeaveGame()
-    {
+
+    public void LeaveGame() {
         Application.Quit();
     }
 }
--- Assets/_scripts/Shoot/GunShot.cs
+++ Assets/_scripts/Shoot/GunShot.cs
@@ -6,12 +6,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class GunShot : NetworkBehaviour
-{
+public class GunShot : NetworkBehaviour {
     private Text ammoUi;
-   // public Animator animator;
+    // public Animator animator;
 
-   public int currentAmmo;
+    public int currentAmmo;
     public int maxAmmo = 30; //in mag
     public int maxReloadAmmo = 90;
     public float fireRate = 15;
@@ -26,47 +25,45 @@ public class GunShot : NetworkBehaviour
     [SerializeField] private AudioClip reloadSound2;
     [SerializeField] private AudioClip reloadSound3;
     [SerializeField] private AudioClip emptyGunSound;
+    [SerializeField] private AudioClip hitmarkerSound;
     [SerializeField] private LayerMask mask;
     private float nextShot;
     public float reloadTime = 3f;
-    
-    
 
-    private void Start()
-    {
+    private Image hitmarkerImage;
+    private float hitmarkerDuration = 0.5f;
+
+
+    private void Start() {
         currentAmmo = maxAmmo;
         ammoUi = GameManager.instance.hud.InGameHUD.GetComponentInChildren<Text>();
+        hitmarkerImage = GameObject.Find("Hitmarker/Image").GetComponent<Image>();
+        hitmarkerImage.color = new Color(1, 1, 1, 0);
     }
 
-    private void Update()
-    {
+    private void Update() {
         ammoUi.text = currentAmmo + " / " + maxReloadAmmo;
         if (isReloading)
             return;
-        if (currentAmmo == 0 && maxReloadAmmo == 0)
-        {
+        if (currentAmmo == 0 && maxReloadAmmo == 0) {
             EmptyGunShot();
             return;
         }
-        if (currentAmmo == 0 || Input.GetKeyDown(KeyCode.R))
-        {
+
+        if (currentAmmo == 0 || Input.GetKeyDown(KeyCode.R)) {
             StartCoroutine(Reload());
             return;
         }
 
-        if (Input.GetButton("Fire1") && Time.time >= nextShot)
-        {
+        if (Input.GetButton("Fire1") && Time.time >= nextShot) {
             nextShot = Time.time + 1f / fireRate;
             Shoot();
         }
-        
     }
 
-    private IEnumerator Reload()
-    {
+    private IEnumerator Reload() {
         isReloading = true;
-        if (currentAmmo == maxAmmo)
-        {
+        if (currentAmmo == maxAmmo) {
             isReloading = false;
             yield break;
         }
@@ -80,69 +77,69 @@ public class GunShot : NetworkBehaviour
         audioSource.clip = reloadSound3;
         audioSource.Play();
 
-        if (maxReloadAmmo >= 30)
-        {
+        if (maxReloadAmmo >= 30) {
             maxReloadAmmo -= maxAmmo - currentAmmo;
             currentAmmo = maxAmmo;
         }
-        else if (maxReloadAmmo < 30 && maxReloadAmmo > 0)
-        {
+        else if (maxReloadAmmo < 30 && maxReloadAmmo > 0) {
             currentAmmo = maxReloadAmmo;
             maxReloadAmmo = 0;
         }
-        
+
         isReloading = false;
     }
 
-    private void EmptyGunShot()
-    {
-        if (currentAmmo == 0 && maxReloadAmmo == 0 && Input.GetButton("Fire1"))
-        {
+    private void EmptyGunShot() {
+        if (currentAmmo == 0 && maxReloadAmmo == 0 && Input.GetButton("Fire1")) {
             audioSource.clip = emptyGunSound;
             audioSource.Play();
         }
     }
 
     [Command]
-    void CmdOnShoot()
-    {
+    void CmdOnShoot() {
         RpcDoMuzzleFlash();
     }
 
     [ClientRpc]
-    void RpcDoMuzzleFlash()
-    {
+    void RpcDoMuzzleFlash() {
         muzzleFlash.Play();
     }
+
     [Client]
-    private void Shoot()
-    {
+    private void Shoot() {
         currentAmmo--;
         int index = Random.Range(0, shootingClips.Length);
         audioSource.clip = shootingClips[index];
         audioSource.Play();
-        
+
         if (!isLocalPlayer)
             return;
-        
+
         CmdOnShoot();
         RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
-        {
-
-
-            if (hit.collider.tag == "Player")
-            {
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) {
+            if (hit.collider.CompareTag("Player")) {
                 CmdPlayerShot(hit.collider.name, weapon.damage);
+                HitActive();
+                Invoke(nameof(HitDisable), hitmarkerDuration);
             }
+
             var ImpactGO = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(ImpactGO, 3f);
         }
     }
 
+    private void HitActive() {
+        hitmarkerImage.color = Color.white;
+    }
+
+    private void HitDisable() {
+        hitmarkerImage.color = new Color(1, 1, 1, 0);
+    }
+
     [Command]
-    void CmdPlayerShot(string _playerID, int _damage)
-    {
+    void CmdPlayerShot(string _playerID, int _damage) {
         Player _player = GameManager.GetPlayer(_playerID);
         _player.RpcTakeDamage(_damage);
     }
