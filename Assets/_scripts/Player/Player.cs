412243bf01d42fde220fbe94794c5a5b36344039
viktord8888 <viktordukacz@onet.pl>
2022-01-20 14:47:54 +0100

    GetComponent on CharacterController

--- Assets/_scripts/Player/Player.cs
+++ Assets/_scripts/Player/Player.cs
@@ -6,14 +6,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class Player : NetworkBehaviour
-{
+public class Player : NetworkBehaviour {
     [SerializeField] private int maxhealth = 100;
     [SyncVar] private int currentHealth;
-    [SerializeField]private Behaviour[] disableOnDeathScripts;
+    [SerializeField] private Behaviour[] disableOnDeathScripts;
     private bool[] wasEnabled;
     [SerializeField] private ParticleSystem blood;
-    [SerializeField] private  GameObject[] disableOnDeathGameObjects;
+    [SerializeField] private GameObject[] disableOnDeathGameObjects;
     [SerializeField] private GameObject postproces;
     private Text hp;
     [SerializeField] private TextMeshProUGUI deathMessage;
@@ -22,28 +21,25 @@ public class Player : NetworkBehaviour
     private GunShot _gunShot;
     private PlayerWeapon weapon;
     float currCountdownValue;
-    [SyncVar] 
-    private bool _isDead = false;
-    public bool isDead
-    {
+    [SyncVar] private bool _isDead = false;
+
+    public bool isDead {
         get { return _isDead; }
         protected set { _isDead = value; }
     }
-    
+
     [SerializeField] private AudioSource deathRespawnAudioSource;
     [SerializeField] private AudioClip deathSound;
     [SerializeField] private AudioClip respawnSound;
-    
+
     [SerializeField] private AudioSource gettingHitAudioSource;
     [SerializeField] private AudioClip[] gettingHitSounds;
 
-    void Update()
-    {
-        if(isLocalPlayer)
+    void Update() {
+        if (isLocalPlayer)
             HpUpdate();
 #if UNITY_EDITOR
-        if (Input.GetKeyDown("k"))
-        {
+        if (Input.GetKeyDown("k")) {
             Debug.Log("DMG");
             RpcTakeDamage(20);
         }
@@ -53,31 +49,28 @@ public class Player : NetworkBehaviour
         // else deathCanvas.SetActive(true);
     }
 
-    private void HpUpdate()
-    {
+    private void HpUpdate() {
         hp.text = "HP: " + currentHealth;
     }
 
-    public void Setup()
-    {
+    public void Setup() {
         this.enabled = true;
-        _gunShot = GetComponent<GunShot>(); 
+        _gunShot = GetComponent<GunShot>();
         canvas = GameObject.FindGameObjectWithTag("Canvas");
         hp = GameManager.instance.hud.HP;
         wasEnabled = new bool[disableOnDeathScripts.Length];
-        for (int i = 0; i < wasEnabled.Length; i++)
-        {
+        for (int i = 0; i < wasEnabled.Length; i++) {
             wasEnabled[i] = disableOnDeathScripts[i].enabled;
         }
 
         SetDefaults();
     }
+
     [ClientRpc]
-    public void RpcTakeDamage(int _dmg)
-    {
+    public void RpcTakeDamage(int _dmg) {
         if (isDead)
             return;
-        if(isLocalPlayer)
+        if (isLocalPlayer)
             StartCoroutine(ShowVignette());
         currentHealth -= _dmg;
         blood.Play();
@@ -86,40 +79,35 @@ public class Player : NetworkBehaviour
         gettingHitAudioSource.Play();
         if (currentHealth <= 0f)
             Die();
-
     }
 
-    private void Die()
-    {
+    private void Die() {
         isDead = true;
-        for (int i = 0; i < disableOnDeathScripts.Length; i++)
-        {
+        for (int i = 0; i < disableOnDeathScripts.Length; i++) {
             disableOnDeathScripts[i].enabled = false;
         }
 
         deathRespawnAudioSource.clip = deathSound;
         deathRespawnAudioSource.Play();
-        
-        if(!isLocalPlayer)
-        foreach (var gameObject in disableOnDeathGameObjects)
-        {
-            gameObject.SetActive(false);
-        }
-        if(isLocalPlayer)
+
+        if (!isLocalPlayer)
+            foreach (var gameObject in disableOnDeathGameObjects) {
+                gameObject.SetActive(false);
+            }
+
+        if (isLocalPlayer)
             DeadCanvasActive();
         StartCoroutine(Respawn());
         StartCoroutine(StartCountdown(5));
-
     }
 
-    private IEnumerator ShowVignette()
-    {
+    private IEnumerator ShowVignette() {
         postproces.SetActive(true);
         yield return new WaitForSeconds(0.25f);
         postproces.SetActive(false);
     }
-    private IEnumerator Respawn()
-    {
+
+    private IEnumerator Respawn() {
         yield return new WaitForSeconds(5f);
         DeadCanvasDeActive();
         SetDefaults();
@@ -129,45 +117,39 @@ public class Player : NetworkBehaviour
         deathRespawnAudioSource.clip = respawnSound;
         deathRespawnAudioSource.Play();
     }
-    public void SetDefaults()
-    {
+
+    public void SetDefaults() {
         isDead = false;
         currentHealth = maxhealth;
 
-        for (int i = 0; i < disableOnDeathScripts.Length; i++)
-        {
+        for (int i = 0; i < disableOnDeathScripts.Length; i++) {
             disableOnDeathScripts[i].enabled = wasEnabled[i];
         }
 
-        if (!isLocalPlayer)
-        {
-            foreach (var gameObject in disableOnDeathGameObjects)
-            {
+        if (!isLocalPlayer) {
+            foreach (var gameObject in disableOnDeathGameObjects) {
                 gameObject.SetActive(true);
-            }   
+            }
         }
     }
 
-    public void DeadCanvasActive()
-    {
+    public void DeadCanvasActive() {
         deathCanvas.SetActive(true);
         canvas.SetActive(false);
     }
-    public void DeadCanvasDeActive()
-    {
+
+    public void DeadCanvasDeActive() {
         deathCanvas.SetActive(false);
         canvas.SetActive(true);
     }
-    public IEnumerator StartCountdown(float countdownValue)
-    {
+
+    public IEnumerator StartCountdown(float countdownValue) {
         currCountdownValue = countdownValue;
-        while (currCountdownValue > 0)
-        {
+        while (currCountdownValue > 0) {
             deathMessage.text = ("Respawn in... " + currCountdownValue);
             Debug.Log("Countdown: " + currCountdownValue);
             yield return new WaitForSeconds(1.0f);
             currCountdownValue--;
         }
     }
-    
 }
