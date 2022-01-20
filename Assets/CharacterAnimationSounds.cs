d3b674d799e58268283dd0f9207adc8abc71525f
viktord8888 <viktordukacz@onet.pl>
2022-01-20 13:55:13 +0100

    Fixed walking sound when not on the ground

--- Assets/CharacterAnimationSounds.cs
+++ Assets/CharacterAnimationSounds.cs
@@ -6,12 +6,13 @@ using Random = UnityEngine.Random;
 
 public class CharacterAnimationSounds : MonoBehaviour {
     [SerializeField] private GameObject player;
+    [SerializeField] private CharacterController playerController;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] footstepClips;
 
     private void PlayerFootstepSound()
     {
-        if (player.GetComponent<Player>().isDead == false) {
+        if (playerController.isGrounded && player.GetComponent<Player>().isDead == false) {
             var index = Random.Range(0, footstepClips.Length);
             audioSource.clip = footstepClips[index];
             audioSource.Play();
