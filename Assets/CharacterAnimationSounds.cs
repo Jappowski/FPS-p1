8ea92ba262ceca99a9db8762f9eae8e2ebcd76dd
viktord8888 <viktordukacz@onet.pl>
2022-01-20 14:47:54 +0100

    GetComponent on CharacterController

--- Assets/CharacterAnimationSounds.cs
+++ Assets/CharacterAnimationSounds.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class CharacterAnimationSounds : MonoBehaviour {
+    private CharacterController playerController;
     [SerializeField] private GameObject player;
-    [SerializeField] private CharacterController playerController;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] footstepClips;
 
+    private void Start() {
+        playerController = GetComponent<CharacterController>();
+    }
+    
     private void PlayerFootstepSound()
     {
         if (playerController.isGrounded && player.GetComponent<Player>().isDead == false) {
