2fa08df9f0b88cd68c4fddc5a3e92614f8d8b1ba
viktord8888 <viktordukacz@onet.pl>
2022-01-23 19:45:26 +0100

    Volume sliders corrections

--- Assets/_scripts/OptionsMenu.cs
+++ Assets/_scripts/OptionsMenu.cs
@@ -1,16 +1,35 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
     [SerializeField] private AudioMixer mainMixer;
     [SerializeField] private AudioMixer ambientMixer;
+    private Slider overallSlider;
+    private Slider ambientSlider;
+
+    private void Start() {
+        overallSlider = GameObject.Find("OptionsUI/Background/OverallVolumeSlider").GetComponent<Slider>();
+        ambientSlider = GameObject.Find("OptionsUI/Background/AmbientVolumeSlider").GetComponent<Slider>();
+    }
 
     public void SetOverallVolume(float volume) {
-        mainMixer.SetFloat("Overall", volume);
+        if (volume > overallSlider.minValue) {
+            mainMixer.SetFloat("Overall", volume);
+        }
+        else {
+            mainMixer.SetFloat("Overall", -80.0f);
+        }
     }
 
     public void SetAmbientVolume(float volume) {
-        ambientMixer.SetFloat("Ambient", volume);
+        if (volume > ambientSlider.minValue) {
+            ambientMixer.SetFloat("Ambient", volume);
+        }
+        else {
+            ambientMixer.SetFloat("Ambient", -80.0f);
+        }
     }
 
     public void SetFullscreen(bool isFullscreen) {
