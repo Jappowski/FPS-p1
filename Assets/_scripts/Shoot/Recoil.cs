0ebbfcef6c03c621c2e241be2aaf528008794a3b
klaudiakarbownik <48563268+klaudiakarbownik@users.noreply.github.com>
2022-01-27 20:09:24 +0100

    Revert "Merge branch 'master' of https://github.com/Jappowski/FPS-p1"
    This reverts commit fca52579b27c891dae96087c25269f5061bae1e3, reversing
    changes made to 5588c25312611e2a3d951ac490c26e9892c4147d.

--- Assets/_scripts/Shoot/Recoil.cs
+++ Assets/_scripts/Shoot/Recoil.cs
@@ -1,6 +1,4 @@
-using System;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class Recoil : MonoBehaviour {
 
@@ -19,15 +17,10 @@ public class Recoil : MonoBehaviour {
     [SerializeField] private float isMovingMultiplyY = 2f;
     [SerializeField] private float isMovingMultiplyZ = 1.3f;
 
-    [SerializeField] private float NotGroundedMultiplyX = 3f;
-    [SerializeField] private float NotGroundedMultiplyY = 3f;
-    [SerializeField] private float NotGroundedMultiplyZ = 1.6f;
-
     [SerializeField] private float snappiness;
     [SerializeField] private float returnSpeed;
 
     [SerializeField] private FPSController fpsController;
-    [SerializeField] private CharacterController characterController;
 
     void Update() {
         targetRotation = Vector3.Lerp(targetRotation, Vector3.zero, returnSpeed * Time.deltaTime);
@@ -36,10 +29,8 @@ public class Recoil : MonoBehaviour {
     }
 
     public void RecoilFire() {
-        if (fpsController.isMoving && characterController.isGrounded) {
+        if (fpsController.isMoving) {
             targetRotation += new Vector3(recoilX * isMovingMultiplyX, Random.Range(-recoilY * isMovingMultiplyY, recoilY * isMovingMultiplyY), Random.Range(-recoilZ * isMovingMultiplyZ, recoilZ * isMovingMultiplyZ));
-        } else if (!characterController.isGrounded) {
-            targetRotation += new Vector3(recoilX * NotGroundedMultiplyX, Random.Range(-recoilY * NotGroundedMultiplyY, recoilY * NotGroundedMultiplyY), Random.Range(-recoilZ * NotGroundedMultiplyZ, recoilZ * NotGroundedMultiplyZ));
         }
         else {
             targetRotation += new Vector3(recoilX, Random.Range(-recoilY, recoilY), Random.Range(-recoilZ, recoilZ));
@@ -47,10 +38,8 @@ public class Recoil : MonoBehaviour {
     }
     
     public void RecoilFireZoom() {
-        if (fpsController.isMoving && characterController.isGrounded) {
+        if (fpsController.isMoving) {
             targetRotation += new Vector3(aimRecoilX * isMovingMultiplyX, Random.Range(-aimRecoilY * isMovingMultiplyY, aimRecoilY * isMovingMultiplyY), Random.Range(-aimRecoilZ * isMovingMultiplyZ, aimRecoilZ * isMovingMultiplyZ));
-        } else if (!characterController.isGrounded) {
-            targetRotation += new Vector3(aimRecoilX * NotGroundedMultiplyX, Random.Range(-aimRecoilY * NotGroundedMultiplyY, aimRecoilY * NotGroundedMultiplyY), Random.Range(-aimRecoilZ * NotGroundedMultiplyZ, aimRecoilZ * NotGroundedMultiplyZ));
         }
         else {
             targetRotation += new Vector3(aimRecoilX, Random.Range(-aimRecoilY, aimRecoilY), Random.Range(-aimRecoilZ, aimRecoilZ));
